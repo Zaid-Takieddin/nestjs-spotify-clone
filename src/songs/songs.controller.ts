d3638cb0d04ce395c33b71ca32012e2e0066447b
Zaid-Takieddin <zaidtakieddin@gmail.com>
2024-10-09 12:38:28 +0300

    add validation pipe to get by id

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { SongsService } from './songs.service';
 import { CreateSongDto } from './dto/create-song.dto';
 import { UpdateSongDto } from './dto/update-song.dto';
@@ -18,8 +27,8 @@ export class SongsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.songsService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.songsService.findOne(id);
   }
 
   @Patch(':id')
